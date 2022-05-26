f6a670b27f8e66b9f5dbe4b7dbace95b9a10540d
gascsd <3311922913@qq.com>
2022-05-26 20:46:16 +0800

    完结

--- 课程设计_张子杨by.zht/课程设计_张子杨/system.c
+++ 课程设计_张子杨by.zht/课程设计_张子杨/system.c
@@ -24,7 +24,7 @@ void create(struct node** phead)//创建链表函数,增加信息
 {
     int n;
     int i;
-    printf("* * * * * * * * * * * * * * * * * * * * * * * * * * *\n");
+    printf("* * * * * * * * * * 建立管理表 * * * * * * * * * * *\n");
     printf("*****************************************************\n");
     printf("请输入记录的个数:");
     scanf("%d", &n);
@@ -234,3 +234,66 @@ void stats(struct node* head)//统计总数量和总金额
     system("pause");
 }
 
+int main()
+{
+    struct node* head = NULL;//定义指向struct node 结构体变量的指针变量
+    int pw = 123;
+    int paw = 0;
+    while (1)
+    {
+        printf("请输入登录密码：\n");
+        scanf("%d", &paw);
+        if (paw == pw)
+        {
+            printf("密码正确\n");
+            while (1)
+            {
+                int n;
+                menu();
+                printf("请输入:>\n");
+                scanf("%d", &n);
+                switch (n)
+                {
+                case 0:
+                    printf("退出:>\n");
+                    return 0;
+                    //break;
+                case 1:
+                    create(&head);//创建链表函数,增加信息
+                    break;
+                case 2:
+                    search(head);//查找信息 
+                    break;
+                case 3:
+                    revise(&head);//修改管理表
+                    break;
+                case 4:
+                    delete(&head);//删除管理表
+                    break;
+                case 5:
+                    print(head);//打印信息
+                    break;
+                case 6:
+                    count(head);//计算每个设备金额
+                    break;
+                case 7:
+                    stats(head);//统计设备总数量和总金额
+                    break;
+                default:
+                    printf("输入错误，请重新输入\n");
+                    break;
+                }
+            }
+        }
+        else
+        {
+            printf("密码错误，请选择下列操作>\n");
+            printf("请输入0退出，或输入其他任意数字重新输入\n");
+            int input = 0;
+            scanf("%d", &input);
+            if (input == 0)
+                return 0;
+        }
+    }
+    return 0;
+}
